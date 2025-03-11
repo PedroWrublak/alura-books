eec74fd952a1eba1c4d8d491cf4fcc6231899ce4
Pedro Wrublak <pedrovili.w@gmail.com>
2025-03-11 13:14:22 -0300

    Utilizando filter para adicionar funcionalidade ao botão livros disponíveis

--- app/methodFilter.js
+++ app/methodFilter.js
@@ -4,6 +4,6 @@ buttons.forEach(btn => btn.addEventListener('click', filterBooks));
 function filterBooks() {
     const btnElement = document.getElementById(this.id);
     const categorie = btnElement.value;
-    let filteredBooks = books.filter(book => book.categoria == categorie);
+    let filteredBooks = categorie == 'disponivel' ? books.filter(book => book.quantidade > 0) : books.filter(book => book.categoria == categorie);
     showBooksOnScreen(filteredBooks);
 }
